1ece8f3fd33e15922565b85fa5d4b644487b8bfd
AlexCaprian <100376334+AlexCaprian@users.noreply.github.com>
2023-12-18 07:55:46 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Definição das constantes para conexão com o banco de dados
-define('HOST', 'localhost'); // Endereço do servidor MySQL (geralmente 'localhost')
-define('USER', 'root');      // Nome de usuário do MySQL
-define('PASS', '');          // Senha do usuário do MySQL
-define('BASE', 'cadastro');  // Nome do banco de dados que você está usando
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', ''); 
+define('BASE', 'cadastro'); 
 
 // Estabelece a conexão com o banco de dados utilizando MySQLi
 $conn = new MySQLi(HOST, USER, PASS, BASE);
@@ -14,6 +14,4 @@ if ($conn->connect_error) {
 }
 
 // Se chegou até aqui, a conexão foi bem-sucedida
-// O objeto $conn agora contém a conexão ativa com o banco de dados
-// Você pode usá-lo para executar consultas e interagir com o banco de dados
 ?>
